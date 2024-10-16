69ad8da689ff637e6dc7dee2aa48fe5f931c6f3b
Fedorov Denis <ssboshen@yandex.ru>
2024-10-16 16:16:44 +0300

    add github-dark/light theme by shiki librrary in content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				compatibilityDate: '2024-04-03',
-				devtools: { enabled: true },
-				modules: [
-				 '@nuxtjs/tailwindcss',
-				 '@nuxtjs/color-mode',
-				 '@nuxt/content'
-				],
-				colorMode: {
-								classSuffix: ''
-				}
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	modules: [
+		'@nuxtjs/tailwindcss',
+		'@nuxtjs/color-mode',
+		'@nuxt/content'
+	],
+	content: {
+		highlight: {
+      theme: {
+        default: 'min-light',
+        dark: 'min-dark',
+      }
+    }
+	},
+	colorMode: {
+		classSuffix: ''
+	}
 })
