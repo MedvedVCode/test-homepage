59234a4d644f2ce4849f88fe9b36916f01b277ed
Fedorov Denis <ssboshen@yandex.ru>
2024-10-20 15:49:50 +0300

    add project-list to markdown content

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,8 +1,11 @@
 <template>
-	<div>
+	<!-- <div>
 		<h2 class="text-2xl font-semibold mb-10">Projects</h2>
 		<ProjectList />
-	</div>
+	</div> -->
+	<article class="prose dark:prose-invert">
+		<ContentDoc path="/projects" />
+	</article>
 </template>
 
 <script setup>
